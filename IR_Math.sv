c2e6c3eeb01b6b51ee331a6bd6838d98d9132302
Nathan <nwoolf@wisc.edu>
2023-12-11 14:40:19 -0600

    latest

--- IR_Math.sv
+++ IR_Math.sv
@@ -51,12 +51,21 @@ module IR_math(clk, rst_n, lft_opn, rght_opn, lft_IR, rght_IR, IR_Dtrm, en_fusio
 	//output to feed into pipelined flop
 	logic signed [11:0] dsrd_hdng_piped_in;
 
+
+	//assign dsrd_hdng_adj = en_fusion ? sum2[11:0] : dsrd_hdng[11:0]; 
+	
+	///////////////////////////////////////////////////////////////
+	// TIMING SOLUTION: Pipeline block 1						// 
+	// 	Slow down input path to PID 						   //
+	// 		-> goto PID module for the other side of this fix //
+	///////////////////////////////////////////////////////////
 	assign dsrd_hdng_piped_in = en_fusion ? sum2[11:0] : dsrd_hdng[11:0]; 
 	//assign dsrd_hdng_adj = dsrd_hdng_piped_in;
+	
 	always_ff @(posedge clk, negedge rst_n)
 		if (!rst_n) 
 			dsrd_hdng_adj <= 12'b0;
 		else 
-			dsrd_hdng_adj <= dsrd_hdng_piped_in; 
+			dsrd_hdng_adj <= dsrd_hdng_piped_in;
 endmodule
 	
